fbf00b04c6a9de9adf1173b4a34607ec6b7092f4
Keyur Sheladeeya <kshelade@my.centennialcollege.ca>
2021-02-27 11:16:02 +0530

    updated db URI of to connect with MongoDB atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports=  //connected the object, you can connect varible or anything 
 {
     //"URI":"mongodb://localhost/book_store" 
-    "URI":"mongodb://localhost/business_contact_list" 
+    //"URI":"mongodb://localhost/business_contact_list" 
+    "URI": "mongodb+srv://Keyur:TVLdGFA3NGhnKUiK@mongodbserver.xlm8j.mongodb.net/business_contact_list?retryWrites=true&w=majority"
 }
